e6e7b3a8ea1e59b02fcb29cbb1a2eea24b97eccd
gustavocabezasx <gustavocabezasx@gmail.com>
2023-07-13 23:29:55 -0600

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.get('/', (req, res) => {
+    res.send("Welcome to my API");
+})
+
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to MongoDB Atlas"))
+    .catch((error) => console.error(error));
+
+app.listen(9000, () => console.log('server listening on port', port));
